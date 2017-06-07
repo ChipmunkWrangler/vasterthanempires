48f7970d53288d15ca5ccda2b9f714673d81a2bd
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-06-07 09:25:13 +0200

    Planets accumulate resources

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Planet : MonoBehaviour {
+	[SerializeField] float secsPerResource = 1;
+	[SerializeField] Text resourceDisplay;
+	[SerializeField] Vector3 labelOffset;
+
+	int resources;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (IncreaseResources ());
+	}
+
+	IEnumerator IncreaseResources() {
+		while (true) {
+			yield return new WaitForSeconds (secsPerResource);
+			++resources;
+			resourceDisplay.text = resources.ToString();
+			resourceDisplay.transform.position = Camera.main.WorldToScreenPoint (transform.position) + labelOffset;
+		}
+	}
+}
