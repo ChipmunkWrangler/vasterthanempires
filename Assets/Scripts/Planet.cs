9f9e546748e40a54dc394deb98cf3447a715b336
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-06-07 09:57:31 +0200

    Planets show resources with delay

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Planet : MonoBehaviour {
-	[SerializeField] float secsPerResource = 1;
+	class ResourceEvent {
+		int resourceChange;
+		float Time;
+	}
+
+	[SerializeField] float secsPerResource = 1f;
 	[SerializeField] Text resourceDisplay;
 	[SerializeField] Vector3 labelOffset;
+	[SerializeField] float secsPerDisplayUpdate = 1f;
+	[SerializeField] float infoSpeedUnitsPerSec = 0.5f;
+	[SerializeField] Transform home;
 
-	int resources;
+//	List<ResourceEvent> resourceEvents;
+	float initialTime;
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (IncreaseResources ());
+		initialTime = Time.time;
+		resourceDisplay.transform.position = Camera.main.WorldToScreenPoint (transform.position) + labelOffset;
+		StartCoroutine (UpdateDisplay ());
 	}
 
-	IEnumerator IncreaseResources() {
+	IEnumerator UpdateDisplay() {
 		while (true) {
-			yield return new WaitForSeconds (secsPerResource);
-			++resources;
-			resourceDisplay.text = resources.ToString();
-			resourceDisplay.transform.position = Camera.main.WorldToScreenPoint (transform.position) + labelOffset;
+			yield return new WaitForSeconds (secsPerDisplayUpdate);
+			float apparentTime = GetApparentTime ();
+			if (apparentTime >= initialTime) {
+				resourceDisplay.text = GetResourcesAtTime (apparentTime).ToString();
+			}
 		}
+	}
+
+	float GetApparentTime() {
+		float distToHome = Vector3.Distance (transform.position, home.position);
+		float transmissionSecsToHome = distToHome / infoSpeedUnitsPerSec;
+		return Time.time - transmissionSecsToHome;
+	}
+	int GetResourcesAtTime(float time) {
+		int resourcesWithoutEvents = Mathf.FloorToInt(time / secsPerResource);
+		return resourcesWithoutEvents;
 	}
 }
