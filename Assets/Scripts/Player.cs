a9a62602369c4623802a229afc59f71d9ec34346
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-06-12 17:27:47 +0200

    Added extensive math comments for apparent time for moving bodies.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -171,7 +171,7 @@ public class Player : NetworkBehaviour {
 
 	void UpdateApparentPosition() {		
 //		print("UpdateApparentPosition");
-		float time = VTEUtil.GetApparentTime (VTEUtil.GetDistToLocalPlayer(GetActualPosition())); 
+		float time = GetApparentTime (VTEUtil.GetLocalPlayer());
 		Vector3? newPosition = GetPositionAt (time);
 		transform.position = newPosition.HasValue ? newPosition.Value : OFFSCREEN;
 //		print ("Apparent pos " + transform.position.ToString() + " Actual pos " + actualPosition.ToString());
@@ -194,5 +194,49 @@ public class Player : NetworkBehaviour {
 		movementEvents.Add (new MovementEvent (startPosition, tgtPosition, tgtPlanet));
 		print ("Creating Movement Event for player " + this.netId.Value.ToString () + " : total = " + movementEvents.Count);
 		print (movementEvents [movementEvents.Count - 1].ToString ());			
+	}
+		
+	void GetApparentTime(Player otherPlayer) { 
+		// for two moving players, their positions given by f(t) = other player's position at time t, and g(t) = my position at time t, 
+		// we want to find the latest t such that the other player can see me now.
+		//                                     => information that leaves g(t) at time t reaches f(NOW) at time NOW
+		//                                     => t + h(t) = NOW, where we define h(t) as the time it takes for information to travel between g(t) and f(NOW).
+		// h(t) = Distance(f(NOW), g(t)) / C, where C is the constant speed of information.
+		// Let (X, Y) := f(NOW) and (x,y) := g(t)
+		// So t + h(t)                                                     = NOW 
+		//        h(t)                                                     = NOW - t
+		// =>     sqrt( (X - x)^2 + (Y - y)^2) / C = NOW - t
+		// =>           (X - x)^2 + (Y - y)^2      = C * (NOW - t)^2
+		// =>    X^2 - 2Xx + x^2 + Y^2 - 2Yy + y^2 = C * (NOW^2 - 2t * NOW + t^2)
+		// For a given movement command, movement is in a straight line at a constant speed.
+		// Thus, g(t) = g(T) + V * (t - T), where T = the time of the command and V is the player's movement velocity.
+		// Let (A,B) := g(T) and (S,W) := V
+		// Then (x,y) = (A,B) + (S,W) * (t - T)
+		// =>  X^2 - 2X(A + S * (t-T)) + (A + S * (t-T))^2                 + Y^2 - 2Y(B + W * (t-T)) + (B + W * (t-T))^2 = C * (NOW^2 - 2t * NOW + t^2)
+		// =>  X^2 - 2XA - 2XS * (t-T) + A^2 + 2AS * (t-T) + (S * (t-T))^2 + ...
+		// =>  X^2 - 2XA - 2XSt + 2XST + A^2 + 2ASt - 2AST + S^2 * (t^2 - 2Tt + T^2)  + ...
+		// =>  X^2 - 2XA - 2XSt + 2XST + A^2 + 2ASt - 2AST + S^2 * t^2 - S^2 * 2Tt + S^2 * T^2  + ...
+		// =>  X^2 - 2XA + 2XST + A^2 - 2AST + S^2 * T^2  - 2XSt + 2ASt - S^2 * 2Tt + S^2 * t^2   + ...
+		// => (X^2 - 2XA + 2XST + A^2 - 2AST + S^2 * T^2) + (2AS - 2XS - S^2 * 2T)t + S^2 * t^2   + ...
+
+// Or
+		// =>           (X - x)^2 + (Y - y)^2      = C * (NOW - t)^2
+		// =>           (X - (A + St - ST))^2 + (Y - (B + Wt - WT))^2 = C * (NOW^2 - 2t * NOW + t^2)
+		// =>           (X - A + ST - St)^2 + (Y - B + WT - Wt)^2 = C * (NOW^2 - 2t * NOW + t^2)
+		// Let K := X - A + ST and L := Y - B + WT. Then
+		// =>           (K - St)^2 + (L - Wt)^2 = C * (NOW^2 - 2t * NOW + t^2)
+		// =>           K^2 - 2KSt + S^2*t^2 + L^2 - 2LWt + W^2*t^2 = C*NOW^2 - 2*C*NOW*t + C*t^2
+		// =>           (K^2 + L^2 - C*NOW^2) + (2C*NOW - 2KS - 2LW)t + (S^2 + W^2 - C)t^2 = 0 (Call this Equation 1)
+		// Let a := (K^2 + L^2 - C*NOW^2), b := (2C*NOW - 2KS - 2LW), c := (S^2 + W^2 - C)
+		// => t = (-b +/- sqrt( b^2 - 4ac)) / 2a  (Quadratic formula)
+
+
+		// If S & W are zero, we get the special case used in VEUtil from Equation 1:
+		// =>           (K^2 + L^2 - C*NOW^2) + (2C*NOW)t - Ct^2 = 0
+		// =>           ((X - A)^2 + (Y - B)^2 - C*NOW^2) + (2C^2*NOW)t - Ct^2 = 0
+		// =>           (Dist(g(NOW), f(NOW)/C)^2 = NOW^2 - (2*NOW)t + t^2 = (NOW - t)^2
+		// =>           Dist(g(NOW), f(NOW)/C = NOW^2 - (2*NOW)t + t^2 = NOW - t
+		// =>  t =  NOW - Dist(g(NOW), f(NOW) / C
+
 	}
 }
